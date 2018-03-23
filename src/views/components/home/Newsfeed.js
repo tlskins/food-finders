bb70d79f1a423c25845ec451f8e745fba6a4e448
Tim Lee <tlee87@gmail.com>
2018-03-23 14:39:15 -0400

    initial newsfeed styling refactor

--- src/views/components/home/Newsfeed.js
+++ src/views/components/home/Newsfeed.js
@@ -8,7 +8,8 @@ import Moment from 'moment'
 
 class NewsFeed extends Component {
   state = {
-    feedItems: []
+    feedItems: [],
+    hoveredItem: undefined,
   }
 
   componentDidMount() {
@@ -23,16 +24,16 @@ class NewsFeed extends Component {
   reloadFeedItems = async () => {
     console.log('reloadingFeedItems')
     const { loadNewsfeed } = this.props
-
     const feedItems = await loadNewsfeed()
     this.setState({ feedItems })
   }
 
   renderFeedItem = item => {
     const { conductedAt, metadata } = item
+
     return (
-      <li className='newsfeed__item--parent'>
-        <div className='newsfeed__item__content'>
+      <div className='newsfeed-item-container'>
+        <div className='newsfeed-item'>
           <div>
             <h3 className='p-header'>
               { metadata.authorName }
@@ -51,22 +52,70 @@ class NewsFeed extends Component {
             }
           </div>
         </div>
-      </li>
+      </div>
+    )
+  }
+
+  renderRatingFeedItem = item => {
+    const { conductedAt, metadata } = item
+    const { foodRating, authorName } = metadata
+    const { rateable, ratee, ratingType, ratingMetrics } = foodRating
+    const rateableName = rateable && rateable.name
+    const rateeName = ratee && ratee.name
+    const ratingTypeName = ratingType && (ratingType.symbol + ratingType.handle)
+
+    return (
+      <div className="newsfeed-item-container">
+        <div className="newsfeed-item">
+          <div className="newsfeed-item-header">
+            { rateableName }
+          </div>
+          <div className="newsfeed-item-sub-header">
+            <span className="newsfeed-item-sub-header-item">{ rateeName }</span>
+          </div>
+          <div className="rating">
+            <span className="rating-component">{ ratingTypeName }</span>
+            <span className="rating-component"> • </span>
+            { ratingMetrics && ratingMetrics.map( (m,i) => {
+              const renderBullet = i !== ratingMetrics.length - 1
+              return (
+                <div>
+                  <span className="rating-component">{ m.symbol + m.handle }</span>
+                  { renderBullet && <span className="rating-component"> • </span> }
+                </div>
+              ) } )
+            }
+          </div>
+          <div>
+            { item.renderContent() }
+            <p className='p-footer'>
+              { authorName }<br />
+              Posted at { Moment(conductedAt).format( 'MM-DD-YY h:mma' ) }
+            </p>
+          </div>
+        </div>
+      </div>
     )
   }
 
   render() {
     const { feedItems } = this.state
-
     const emptyFeed = feedItems.length === 0
     console.log('feedItems=',feedItems)
 
     return (
       <div>
-        <ul className='newsfeed'>
+        <div className='newsfeed'>
           { emptyFeed && `Empty Feed!` }
-          { feedItems.map( (f,i) => this.renderFeedItem(f) ) }
-        </ul>
+          { feedItems.map( (f,i) => {
+            if ( f.metadata && f.metadata.foodRating ) {
+              return this.renderRatingFeedItem(f)
+            }
+            else {
+              return this.renderFeedItem(f)
+            }
+          } ) }
+        </div>
       </div>
     )
   }
